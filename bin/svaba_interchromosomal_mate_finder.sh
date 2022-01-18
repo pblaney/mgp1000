9bf7d75126de9e77740c41018a8d073f1747ddfa
pblaney <patrick.blaney@nyulangone.org>
2022-01-18 17:26:57 -0500

    fixed issue with grep

--- bin/svaba_interchromosomal_mate_finder.sh
+++ bin/svaba_interchromosomal_mate_finder.sh
@@ -22,10 +22,10 @@ for id in `grep -v '^#' $svaba_processed_vcf | cut -f 3 | sed 's|:.||' | sort -k
 
 				if [[ $sub_id == 1 ]]; then
 
-					zgrep "${id}:2\t" $svaba_unprocessed_vcf
+					zgrep -P "${id}:2\t" $svaba_unprocessed_vcf
 				else
 					
-					zgrep "${id}:1\t" $svaba_unprocessed_vcf
+					zgrep -P "${id}:1\t" $svaba_unprocessed_vcf
 				fi
 
 			fi
