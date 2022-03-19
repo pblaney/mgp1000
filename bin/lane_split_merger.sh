e9b4285176144c307c562227ffb363547ded3135
pblaney <patrick.blaney@nyulangone.org>
2022-03-18 21:33:15 -0400

    helper process for dealing with lane split input fastqs

---
+++ bin/lane_split_merger.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Given an input directory, merge all lane-split FASTQ files per R1/R2 sets
+
+inputDirectory=$1
+
+outputDirectory=$2
+
+mkdir -p "${outputDirectory}"
+find -L "${inputDirectory}" -maxdepth 8 -type f -name "*.fastq.gz" \
+| \
+while read F; 
+do 
+	basename $F | sed -E 's|_00[1234].fastq.gz||';
+done \
+| \
+sort \
+| \
+uniq \
+| while read P;
+do
+	find -L "${inputDirectory}" -maxdepth 8 -type f -name "${P}_00*.fastq.gz" -exec cat '{}' ';' > "${outputDirectory}/${P}.merged.fastq.gz";
+done
