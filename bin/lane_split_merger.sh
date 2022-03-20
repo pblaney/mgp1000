957a590d532f2b196e3acf02443efe89a0697c3b
pblaney <patrick.blaney@nyulangone.org>
2022-03-19 20:30:30 -0400

    changed cat to find command

--- bin/lane_split_merger.sh
+++ bin/lane_split_merger.sh
@@ -19,6 +19,6 @@ uniq \
 | \
 while read P
 do
-	cat input/${P}_00*.fastq.gz > "${P}.merged.fastq.gz"
+	find -L "${inputDirectory}" -maxdepth 8 -type f -name "${P}_00*.fastq.gz" -exec cat '{}' ';' > "${P}.merged.fastq.gz"
 	mv "${P}.merged.fastq.gz" "${outputDirectory}"
 done
