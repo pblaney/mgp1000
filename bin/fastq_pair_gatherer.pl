231cd09ba50311012bd6fd9b316a2030566435dd
pblaney <patrick.blaney@nyulangone.org>
2022-03-19 23:34:01 -0400

    updated fastq gather input regex

--- bin/fastq_pair_gatherer.pl
+++ bin/fastq_pair_gatherer.pl
@@ -15,7 +15,7 @@ sub main {
 	chomp($input_dir);
 
 	# Find all the FASTQs in the input directory
-	my $fastq_name_globs = "-name '*_R1_0*.fastq.gz' -or -name '*_R1.fastq.gz'";
+	my $fastq_name_globs = "-name '*_R1_0*.fastq.gz' -or -name '*_R1.*fastq.gz'";
 	my $fastq_find_command = "find -L $input_dir -maxdepth 8 -type f $fastq_name_globs | LC_ALL=C sort";
 	my @input_fastqs = `$fastq_find_command`;
 
@@ -33,7 +33,7 @@ sub main {
 		# Generate the R2 FASTQ filename based on R1
 		my $read2_fastq = $read1_fastq;
 		$read2_fastq =~ s/(.*)_R1_0([0-9]+.fastq.gz)/${1}_R2_0${2}/;
-		$read2_fastq =~ s/(.*)_R1.fastq.gz/${1}_R2.fastq.gz/;
+		$read2_fastq =~ s/(.*)_R1.fastq.gz/${1}_R2.*fastq.gz/;
 
 		# Remove directory structure from reads
 		$read1_fastq =~ s/.*\///;
