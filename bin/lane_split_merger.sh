2ba5815e5b13d515bbb31241c11554e42f6a0097
pblaney <patrick.blaney@nyulangone.org>
2022-03-20 22:28:09 -0400

    removed cumbersome unzip/sort/rezip process as likely not needed

--- bin/lane_split_merger.sh
+++ bin/lane_split_merger.sh
@@ -6,7 +6,6 @@ inputDirectory=$1
 outputDirectory=$2
 
 mkdir -p "${outputDirectory}"
-mkdir -p tmp/
 ls -1 ${inputDirectory}/*.fastq.gz \
 | \
 while read F
@@ -20,9 +19,6 @@ uniq \
 | \
 while read P
 do
-	find -L "${inputDirectory}" -maxdepth 8 -type f -name "${P}_00*.fastq.gz" -exec cat '{}' ';' > "${P}.unsorted.merged.fastq.gz"
-	gunzip "${P}.unsorted.merged.fastq.gz"
-	fastq-sort --temporary-directory tmp/ --idn "${P}.unsorted.merged.fastq" | bgzip > "${P}.merged.fastq.gz"
+	find -L "${inputDirectory}" -maxdepth 8 -type f -name "${P}_00*.fastq.gz" -exec cat '{}' ';' | bgzip > "${P}.merged.fastq.gz"
 	mv "${P}.merged.fastq.gz" "${outputDirectory}"
-	rm "${P}.unsorted.merged.fastq"
 done
