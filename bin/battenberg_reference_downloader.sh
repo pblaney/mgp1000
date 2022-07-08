edefd49a25c426f89dff0cc7499725c8c7c458df
pblaney <patrick.blaney@nyulangone.org>
2022-07-07 20:00:05 -0400

    adjustment to download placement

--- bin/battenberg_reference_downloader.sh
+++ bin/battenberg_reference_downloader.sh
@@ -40,6 +40,7 @@ battenbergRefDownload() {
   	# Beagle5
   	wget -q -O beagle_chr.zip "https://ora.ox.ac.uk/objects/uuid:08e24957-7e76-438a-bd38-66c48008cf52/download_file?file_format=&safe_filename=beagle_chr.zip&type_of_work=Dataset" && \
   	unzip beagle_chr.zip
+  	rm beagle_chr.zip && \
   	mv beagle/ beagle5/ && \
   	cd beagle5/ && \
   	mkdir -p tmp/ && \
