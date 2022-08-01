999bd499fb19e2bd153a93089e029173ca5785d4
pblaney <patrick.blaney@nyulangone.org>
2022-07-31 23:48:49 -0400

    updated lane split merge script for more naming conventions

--- bin/lane_split_merger.sh
+++ bin/lane_split_merger.sh
@@ -6,21 +6,53 @@ inputDirectory=$1
 outputDirectory=$2
 
 mkdir -p "${outputDirectory}"
-ls -1 ${inputDirectory}/*.fastq.gz \
+ls -1 ${inputDirectory}/*.f*q.gz \
 | \
-while read F
-do 
-	basename $F | sed -E 's|_00[1234].fastq.gz||'
+while read fastqPath
+do
+	# Find base naming convention
+	basename $fastqPath | sed -E 's|\.f.*q.gz||'
+done \
+| \
+sort \
+| \
+while read fastqBasename
+do
+	# Test different lane split conventions
+	if [[ ${fastqBasename} =~ _L00[1-9]_R[12]_.*$ ]]; then
+
+		echo ${fastqBasename} | sed -E 's|_L00[1-9]|_L00\*|'
+
+	elif [[ ${fastqBasename} =~ _L00[1-9]_R[12]$ ]]; then
+
+		echo ${fastqBasename} | sed -E 's|_L00[1-9]|_L00\*|'
+
+	elif [[ ${fastqBasename} =~ _R[12]_00[1-9] ]]; then
+
+		echo ${fastqBasename} | sed -E 's|_00[1-9]$|_00\*|'
+	fi
 done \
 | \
 sort \
 | \
 uniq \
 | \
-while read P
+while read laneSplitRegex
 do
-	splitFiles=$(find -L "${inputDirectory}" -maxdepth 8 -type f -name "${P}_00*.fastq.gz" | sort)
-    mergeCmd="cat ${splitFiles} > ${P}.merged.fastq.gz"
-    eval ${mergeCmd}
-	mv "${P}.merged.fastq.gz" "${outputDirectory}"
+	# Concat lane split FASTQs
+	splitFiles=$(find -L "${inputDirectory}" -maxdepth 8 -type f -name "${laneSplitRegex}.fastq.gz" | sort)
+
+	if [[ ${laneSplitRegex} =~ _L00\* ]]; then
+
+		laneMergedFastqBasename=$(echo ${laneSplitRegex} | sed -E 's|_L00\*||')
+
+	elif [[ ${laneSplitRegex} =~ _00\* ]]; then
+
+		laneMergedFastqBasename=$(echo ${laneSplitRegex} | sed -E 's|_00\*||')
+
+	fi
+
+	mergeCmd="cat ${splitFiles} > ${laneMergedFastqBasename}.merged.fastq.gz"
+	eval ${mergeCmd}
+	mv "${laneMergedFastqBasename}.merged.fastq.gz" "${outputDirectory}"
 done
