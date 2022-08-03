4a772cc5e012679b8e997fb6272c37d81289d836
pblaney <patrick.blaney@nyulangone.org>
2022-08-03 00:54:46 -0400

    improve regex in split merge

--- bin/lane_split_merger.sh
+++ bin/lane_split_merger.sh
@@ -19,13 +19,13 @@ sort \
 while read fastqBasename
 do
 	# Test different lane split conventions
-	if [[ ${fastqBasename} =~ _L00[1-9]_R[12]_.*$ ]]; then
+	if [[ ${fastqBasename} =~ _L00[1-9]_R[12]+ ]]; then
 
-		echo ${fastqBasename} | sed -E 's|_L00[1-9]|_L00\*|'
+		echo ${fastqBasename} | sed -E 's|_L00[1-9]_R|_L00\*_R|'
+	
+	elif [[ ${fastqBasename} =~ _00[1-9]_R[12]+ ]]; then
 
-	elif [[ ${fastqBasename} =~ _L00[1-9]_R[12]$ ]]; then
-
-		echo ${fastqBasename} | sed -E 's|_L00[1-9]|_L00\*|'
+		echo ${fastqBasename} | sed -E 's|_00[1-9]_R|_00\*_R|'
 
 	elif [[ ${fastqBasename} =~ _R[12]_00[1-9] ]]; then
 
@@ -40,16 +40,19 @@ uniq \
 while read laneSplitRegex
 do
 	# Concat lane split FASTQs
-	splitFiles=$(find -L "${inputDirectory}" -maxdepth 8 -type f -name "${laneSplitRegex}.fastq.gz" | sort)
+	splitFiles=$(find -L "${inputDirectory}" -maxdepth 8 -type f -name "${laneSplitRegex}.f*q.gz" | sort)
 
-	if [[ ${laneSplitRegex} =~ _L00\* ]]; then
+	if [[ ${laneSplitRegex} =~ _L00\*_R ]]; then
 
 		laneMergedFastqBasename=$(echo ${laneSplitRegex} | sed -E 's|_L00\*||')
 
-	elif [[ ${laneSplitRegex} =~ _00\* ]]; then
+	elif [[ ${laneSplitRegex} =~ _00\*_R ]]; then
 
 		laneMergedFastqBasename=$(echo ${laneSplitRegex} | sed -E 's|_00\*||')
 
+	elif [[ ${laneSplitRegex} =~ _R[12]_00\* ]]; then
+
+		laneMergedFastqBasename=$(echo ${laneSplitRegex} | sed -E 's|_00\*||')
 	fi
 
 	mergeCmd="cat ${splitFiles} > ${laneMergedFastqBasename}.merged.fastq.gz"
