92be4243600ad9e8bfdc673c0423d85bd5e636d7
pblaney <patrick.blaney@nyulangone.org>
2022-11-17 15:45:11 -0500

    adjustments to execution script after battenberg download updates

--- bin/battenberg_executor.sh
+++ bin/battenberg_executor.sh
@@ -24,6 +24,10 @@ sed -E 's|\$\{REF_PATH\}|'${PWD}'/battenberg_reference|g' battenberg_reference/i
 # Second, update the path of the Beagle5 base directory
 cat /opt/battenberg/inst/example/battenberg_wgs.R | \
 sed 's|BEAGLE_BASEDIR = \".*|BEAGLE_BASEDIR = \"'${PWD}'/battenberg_reference\"|' | \
+sed 's|1kg.phase3.v5a_GRCh38nounref_allele_index_|1kg.phase3.v5a_GRCh38nounref_allele_index_chr|' | \
+sed 's|1kg.phase3.v5a_GRCh38nounref_loci_|1kg.phase3.v5a_GRCh38nounref_loci_chr|' | \
+sed 's|1000G_GC_|1000G_GC_chr|' | \
+sed 's|1000G_RT_|1000G_RT_chr|' | \
 sed 's|CHROM_COORD_FILE = \".*|CHROM_COORD_FILE = \"/opt/battenberg/chromosome_coordinates_hg38.txt\"|' | \
 sed 's|MIN_NORMAL_DEPTH = 10|MIN_NORMAL_DEPTH = '${minDepth}'|' > battenberg_wgs.R
 
