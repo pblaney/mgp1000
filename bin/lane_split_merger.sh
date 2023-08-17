9bdaccccef863586c3d149fcda8bee6d4f9ad6ef
pblaney <patrick.blaney@nyulangone.org>
2023-08-17 13:02:53 -0400

    code style and resource improvements for nyu

--- bin/lane_split_merger.sh
+++ bin/lane_split_merger.sh
@@ -9,55 +9,49 @@ outputDirectory=$3
 
 mkdir -p "${outputDirectory}"
 ls -1 ${workDirectory}/*.f*q.gz \
-| \
-while read fastqPath
-do
-	# Find base naming convention
-	basename $fastqPath | sed -E 's|\.f.*q.gz||'
-done \
-| \
-sort \
-| \
-while read fastqBasename
-do
-	# Test different lane split conventions
-	if [[ ${fastqBasename} =~ _L00[1-9]_R[12]+ ]]; then
+| while read fastqPath
+	do
+		# Find base naming convention
+		basename $fastqPath | sed -E 's|\.f.*q.gz||'
+	done \
+	| sort \
+	| while read fastqBasename
+		do
+			# Test different lane split conventions
+			if [[ ${fastqBasename} =~ _L00[1-9]_R[12]+ ]]; then
 
-		echo ${fastqBasename} | sed -E 's|_L00[1-9]_R|_L00\*_R|'
+				echo ${fastqBasename} | sed -E 's|_L00[1-9]_R|_L00\*_R|'
 	
-	elif [[ ${fastqBasename} =~ _00[1-9]_R[12]+ ]]; then
+			elif [[ ${fastqBasename} =~ _00[1-9]_R[12]+ ]]; then
 
-		echo ${fastqBasename} | sed -E 's|_00[1-9]_R|_00\*_R|'
+				echo ${fastqBasename} | sed -E 's|_00[1-9]_R|_00\*_R|'
 
-	elif [[ ${fastqBasename} =~ _R[12]_00[1-9] ]]; then
+			elif [[ ${fastqBasename} =~ _R[12]_00[1-9] ]]; then
 
-		echo ${fastqBasename} | sed -E 's|_00[1-9]$|_00\*|'
-	fi
-done \
-| \
-sort \
-| \
-uniq \
-| \
-while read laneSplitRegex
-do
-	# Concat lane split FASTQs
-	splitFiles=$(find -L "${inputDirectory}" -maxdepth 8 -type f -name "${laneSplitRegex}.f*q.gz" | sort)
+				echo ${fastqBasename} | sed -E 's|_00[1-9]$|_00\*|'
+			fi
+		done \
+		| sort \
+		| uniq \
+		| while read laneSplitRegex
+			do
+				# Concat lane split FASTQs
+				splitFiles=$(find -L "${inputDirectory}" -maxdepth 8 -type f -name "${laneSplitRegex}.f*q.gz" | sort)
 
-	if [[ ${laneSplitRegex} =~ _L00\*_R ]]; then
+				if [[ ${laneSplitRegex} =~ _L00\*_R ]]; then
 
-		laneMergedFastqBasename=$(echo "$laneSplitRegex" | sed -E 's|_L00\*||')
+					laneMergedFastqBasename=$(echo "$laneSplitRegex" | sed -E 's|_L00\*||')
 
-	elif [[ ${laneSplitRegex} =~ _00\*_R ]]; then
+				elif [[ ${laneSplitRegex} =~ _00\*_R ]]; then
 
-		laneMergedFastqBasename=$(echo "$laneSplitRegex" | sed -E 's|_00\*||')
+					laneMergedFastqBasename=$(echo "$laneSplitRegex" | sed -E 's|_00\*||')
 
-	elif [[ ${laneSplitRegex} =~ _R[12]_00\* ]]; then
+				elif [[ ${laneSplitRegex} =~ _R[12]_00\* ]]; then
 
-		laneMergedFastqBasename=$(echo "$laneSplitRegex" | sed -E 's|_00\*||')
-	fi
+					laneMergedFastqBasename=$(echo "$laneSplitRegex" | sed -E 's|_00\*||')
+				fi
 
-	mergeCmd="cat ${splitFiles} > ${laneMergedFastqBasename}.merged.fastq.gz"
-	eval ${mergeCmd}
-	mv "${laneMergedFastqBasename}.merged.fastq.gz" "${outputDirectory}"
-done
+				mergeCmd="cat ${splitFiles} > ${laneMergedFastqBasename}.merged.fastq.gz"
+				eval ${mergeCmd}
+				mv "${laneMergedFastqBasename}.merged.fastq.gz" "${outputDirectory}"
+			done
